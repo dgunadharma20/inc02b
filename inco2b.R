720d25fa2f43cf75980381f018238c2153620154
Darren Gunadharma <dgunadharma20@gmail.com>
2021-04-08 17:32:55 -0700

    edits

--- inco2b.R
+++ inco2b.R
@@ -5,4 +5,19 @@ lightDay <- c*24
 lightYear <- c*365
 cat("length of year:", lightYear, "\n")
 
-#299 792 458
+paste("Hi", lightYear)
+
+a <- 1
+print(a)
+
+sqrt(4)
+
+min(1, 2, pi)
+
+add <- function(x, y) {
+  x+y
+}
+
+lightyear <- function(ly) {
+  ly * 9.461e+12
+}
